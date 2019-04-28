76bd8132ecc1a4f22a7ac24ff35ecba42f7aeb25
zmj0920 <mj960920@163.com>
2019-04-28 16:43:56 +0800

    初始化象目框架

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect" class="nav">
+      <el-menu-item>
+        <img src="./img/logo.v3.png" alt class="logo">
+      </el-menu-item>
+      <el-submenu index="3">
+        <template slot="title">设计素材</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+        <el-submenu index="2-4">
+          <template slot="title">选项4</template>
+          <el-menu-item index="2-4-1">选项1</el-menu-item>
+          <el-menu-item index="2-4-2">选项2</el-menu-item>
+          <el-menu-item index="2-4-3">选项3</el-menu-item>
+        </el-submenu>
+      </el-submenu>
+      <el-menu-item index="1">
+        <a href="#" target class="a-none">PPT模板</a>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <a href="#" target class="a-none">设计素材</a>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <a href="#" target class="a-none">平面模板</a>
+      </el-menu-item>
+      <el-menu-item index="5">
+        <a href="#" target class="a-none">图片视频</a>
+      </el-menu-item>
+      <el-menu-item index="6">
+        <a href="#" target class="a-none">PPT图表</a>
+      </el-menu-item>
+
+      
+      <el-menu-item class="w-nav"></el-menu-item>
+
+
+      <el-menu-item>
+        <el-button-group>
+          <el-button type="primary" plain>登陆</el-button>
+          <el-button type="success" plain>注册</el-button>
+        </el-button-group>
+      </el-menu-item>
+    </el-menu>
+    <router-view></router-view>
+
+    <!--尾部-->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      activeIndex2: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+<style scoped>
+.nav li {
+  margin: 0 auto 0;
+}
+.logo {
+  width: 100px;
+}
+.a-none {
+  text-decoration: none;
+}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <el-carousel indicator-position="outside" :interval="2000" arrow="always" height="400">
+      <el-carousel-item v-for="item in listimg_url" :key="item.id">
+        <img :src=" item.img_url" alt>
+      </el-carousel-item>
+    </el-carousel>
+
+    <el-row>
+   <el-col :span="3" v-for="(o, index) in 10" :key="o" :offset="index > 0 ? 1 : 0">
+    <el-card :body-style="{ padding: '0px' }">
+      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+      <div style="padding: 14px;">
+        <span>好吃的汉堡</span>
+        <div class="bottom clearfix">
+          <time class="time">{{ currentDate }}</time>
+          <el-button type="text" class="button">操作按钮</el-button>
+        </div>
+      </div>
+    </el-card>
+  </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listimg_url: [
+        { id: 1, img_url: require("./../img/banner1.jpg") },
+        { id: 2, img_url: require("./../img/banner2.jpg") },
+        { id: 3, img_url: require("./../img/banner3.jpg") }
+      ],
+       currentDate: new Date()
+    };
+  }
+};
+</script>
+
+<style>
+  .time {
+    font-size: 13px;
+    color: #999;
+  }
+  
+  .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+  }
+
+  .button {
+    padding: 0;
+    float: right;
+  }
+
+  .image {
+    width: 100%;
+    display: block;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+      display: table;
+      content: "";
+  }
+  
+  .clearfix:after {
+      clear: both
+  }
+</style>
